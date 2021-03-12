f9cb2a95687388094fdc164e2ff11f8fee2fa509
NahidAkhtar84 <nahid.ibne.akhtar@gmail.com>
2021-03-13 02:56:27 +0600

    views updated

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from event_booking.models import Events
-
+import datetime
+from django.core.paginator import Paginator, EmptyPage
+from admin_site.models import BlogPost
 # Create your views here.
 
 def Index(request):
     all_events = Events.objects.filter(status='accepted')
+    recent_events = Events.objects.filter(status='accepted').filter(start__gte = datetime.datetime.now()).order_by('-start')
+    firstblog = BlogPost.objects.all().order_by('date')[0]
+    blogs = BlogPost.objects.all().order_by('date')[1:3]
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(recent_events, 3)
+
+    try:
+        recent_events = paginator.page(page)
+    except EmptyPage:
+        recent_events = paginator.page(paginator.num_pages)
+
+
     context = {
         "events": all_events,
+        "recent_events": recent_events,
+        "blogs": blogs,
+        "firstblog": firstblog,
     }
     return render(request, 'index/index.html', context)
